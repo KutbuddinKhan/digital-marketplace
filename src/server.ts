5d8001d6d16d2cbb949d7a638da222f2a36e5a2a
Kutbuddin khan <Kutbuddinkhan82@gmail.com>
2023-12-01 14:52:40 +0530

    updated

--- src/server.ts
+++ src/server.ts
@@ -47,6 +47,22 @@ const start = async () => {
     },
   });
 
+  const cartRouter = express.Router()
+
+  cartRouter.use(payload.authenticate)
+
+  cartRouter.get("/", (req, res) => {
+    const request = req as PayloadRequest
+
+    if(!request.user) return res.redirect('/sign-in?origin-cart')
+
+    const parseUrl = parse(req.url, true)
+
+    return nextApp.render(req, res, "/cart", parseUrl.query)
+  })
+
+  app.use('/cart', cartRouter)
+
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info(
